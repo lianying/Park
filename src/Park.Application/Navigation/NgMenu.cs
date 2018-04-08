e3da771c9012336a791d4fc0d0eeda8e40f37e4e
yang524433 <542656180@qq.com>
2018-04-08 22:16:52 +0800

    新增NgMenu

--- src/Park.Application/Navigation/NgMenu.cs
+++ src/Park.Application/Navigation/NgMenu.cs
@@ -7,7 +7,23 @@ using System.Threading.Tasks;
 namespace Park.Navigation
 {
     public class NgMenu
-    {
+    { 
+        /** 文本 */
+        public string text { get; set; }
+        /** angular 路由 */
+        public string link { get; set; }
+        /** 链接 target   '_blank' | '_self' | '_parent' | '_top' */
+        public string target { get; set; }
+        /** 图标 */
+        public string icon { get; set; }
+        /** 是否隐藏菜单 */
+        public bool hide { get; set; }
+        /** i18n主键 */
+        public string i18n { get; set; }
+        /** 是否快捷菜单项 */
+        public bool shortcut { get; set; }
+        /** ACL配置，若导入 `@delon/acl` 时自动有效 */
+        public object acl { get; set; }
 
     }
 }
