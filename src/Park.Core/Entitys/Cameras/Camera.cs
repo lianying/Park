523aae776759a242d9a3b2db5af26f5e9fb32cce
yang524433 <542656180@qq.com>
2018-04-24 23:49:12 +0800

    添加摄像机

---
+++ src/Park.Core/Entitys/Cameras/Camera.cs
@@ -0,0 +1,33 @@
+﻿using Abp.Domain.Entities;
+using Park.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Park.Entitys.Cameras
+{
+    public class Camera:Entity<long>,IMayHaveTenant
+    {
+        public virtual string Ip { get; set; }
+
+        public virtual long Port { get; set; }
+
+        public virtual string UserName { get; set; }
+
+        public virtual string Password { get; set; }
+        /// <summary>
+        /// 设备厂商
+        /// </summary>
+        public virtual EquipmentManufacturers EquipmentManufacturers { get; set; }
+
+        [ForeignKey("EntranceId")]
+        public virtual ParkEntrances.ParkEntrances ParkEntrance { get; set; }
+        
+        public virtual long EntranceId { get; set; }
+
+        public virtual int? TenantId { get; set; }
+    }
+}
---
+++ src/Park.Core/Enum/EquipmentManufacturers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Park.Enum
+{
+   public enum EquipmentManufacturers
+    {
+        [Description("蓝卡")]
+        LanKa,
+        [Description("海康")]
+        HaiKang
+    }
+}
